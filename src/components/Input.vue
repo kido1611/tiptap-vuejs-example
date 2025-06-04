1733dcc9b8ce334653ef50c45f82679e70e53133
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-06-04 17:28:28 +0700

    chore: use defineModel

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,22 +1,10 @@
 <template>
   <input
-    v-model="proxyModel"
+    v-model="model"
     class="rounded-md border border-gray-400 px-3 py-2 text-gray-800 focus:ring-blue-800"
   />
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue"
-const props = defineProps<{
-  modelValue: string | number | undefined
-}>()
-
-const emit = defineEmits<{
-  (e: "update:modelValue", value: string | number): void
-}>()
-
-const proxyModel = computed({
-  get: () => props.modelValue ?? "",
-  set: (value: string | number): void => emit("update:modelValue", value),
-})
+const model = defineModel<string>()
 </script>
